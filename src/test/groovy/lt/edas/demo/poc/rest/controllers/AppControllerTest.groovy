71715e048199d26a50e7da9f14a98fb3cda7ba2b
Dum6o <edwardasas@gmail.com>
2021-02-26 09:19:05 +0200

    Add test GitHub action

--- src/test/groovy/lt/edas/demo/poc/rest/controllers/AppControllerTest.groovy
+++ src/test/groovy/lt/edas/demo/poc/rest/controllers/AppControllerTest.groovy
@@ -1,7 +1,5 @@
 package lt.edas.demo.poc.rest.controllers
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 // @SpringBootTest
